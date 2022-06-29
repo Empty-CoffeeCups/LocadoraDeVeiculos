e892ba33b4223e36e8cf5f6e1f64719b6275967f
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-06-29 15:54:53 -0300

    Implementado Camada De Aplicação - Serviço Grupo De Veículos

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Aplicacao/ModuloGrupoDeVeiculos/ServicoGrupoDeVeiculos.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.Results;
+using locadoraDeVeiculos.Infra.ModuloFuncionario;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoDeVeiculos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoDeVeiculos
+{
+    public class ServicoGrupoDeVeiculos
+    {
+        private RepositorioGrupoDeVeiculosEmBancoDados repositorioGrupoVeiculos;
+
+        public ServicoGrupoDeVeiculos(RepositorioGrupoDeVeiculosEmBancoDados repositorioGrupoVeiculos)
+        {
+            this.repositorioGrupoVeiculos = repositorioGrupoVeiculos;
+        }
+
+        public ValidationResult Inserir(GrupoDeVeiculos arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidationResult Editar(GrupoDeVeiculos arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
