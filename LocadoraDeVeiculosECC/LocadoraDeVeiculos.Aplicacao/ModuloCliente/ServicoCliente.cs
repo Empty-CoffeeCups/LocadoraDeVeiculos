8b459e6b273e7d2d7d2ebf4d18a75e5699afe64b
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-06-29 16:00:34 -0300

    Implementado Camada De Aplicação - Serviço Cliente

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Aplicacao/ModuloCliente/ServicoCliente.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation.Results;
+using locadoraDeVeiculos.Infra.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloCliente
+{
+    public class ServicoCliente
+    {
+        private RepositorioClienteEmBancoDados repositorioCliente;
+
+        public ServicoCliente(RepositorioClienteEmBancoDados repositorioCliente)
+        {
+            this.repositorioCliente = repositorioCliente;
+        }
+
+        public ValidationResult Inserir(Cliente arg)
+        {
+            var resultadoValidacao = ValidarCliente(arg);
+
+            if (resultadoValidacao.IsValid)
+                repositorioCliente.Editar(arg);
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Editar(Cliente arg)
+        {
+            var resultadoValidacao = ValidarCliente(arg);
+
+            if (resultadoValidacao.IsValid)
+                repositorioCliente.Editar(arg);
+
+            return resultadoValidacao;
+        }
+
+        private ValidationResult ValidarCliente(Cliente arg)
+        {
+            ValidadorCliente validador = new ValidadorCliente();
+
+            var resultadoValidacao = validador.Validate(arg);
+
+            return resultadoValidacao;
+        }
+    }
+}
