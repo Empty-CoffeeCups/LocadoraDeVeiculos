29e4f1f25c523cec19801f3e8a95110cfb8f76b2
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-06-30 14:19:42 -0300

    Criação de Pastas e Arquivos de Cada Módulo dos testes de integração (ainda deve ser implementado)

---
+++ LocadoraDeVeiculosECC/LocadoraVeiculos.Infra.BancoDados/ModuloTaxa/RepositorioTaxaEmBancoDadosTest.cs
@@ -0,0 +1,19 @@
+﻿using locadoraDeVeiculos.Infra.Compartilhado;
+using locadoraDeVeiculos.Infra.ModuloTaxas;
+using LocadoraDeVeiculos.Dominio.ModuloTaxas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.BancoDados.ModuloTaxa
+{
+    [TestClass]
+    public class RepositorioTaxaEmBancoDadosTest
+    {
+
+
+    }
+}
