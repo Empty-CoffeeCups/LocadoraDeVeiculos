207cb2606f19080cba2532320a8ad24b9ef1a997
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-01 15:50:18 -0300

    Adicionado classe enum Tipo Cliente no Domínio

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos/ModuloCliente/TipoCliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente
+{
+    public enum TipoCliente
+    {
+        [Description("Pessoa física")]
+        PessoaFisica,
+
+        [Description("Pessoa jurídica")]
+        PessoaJuridica,
+    }
+}
