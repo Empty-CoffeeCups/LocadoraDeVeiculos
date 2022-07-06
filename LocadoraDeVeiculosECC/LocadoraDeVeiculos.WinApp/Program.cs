b9fc59a0727cba8ae8d7472437d037b206deb91f
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-06 16:02:33 -0300

    Implementado Infra de Log e instanciado na classe program

--- LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/Program.cs
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using LocadoraDeVeiculos.WinFormsApp.Compartilhado;
+﻿using LocadoraDeVeiculos.Infra.Logging;
+using LocadoraDeVeiculos.WinFormsApp.Compartilhado;
 using System;
 using System.Windows.Forms;
 
@@ -12,6 +13,7 @@ namespace LocadoraDeVeiculos.WinApp
         [STAThread]
         static void Main()
         {
+            ConfiguracaoLogsLocadora.ConfigurarEscritaLogs();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
