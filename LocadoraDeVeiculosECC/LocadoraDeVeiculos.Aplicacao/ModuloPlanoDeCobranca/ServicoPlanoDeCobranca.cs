9bd4c5f88e87531d29ce3150968048b873686936
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-07 15:31:39 -0300

    Servico Plano de Cobranca implementado na camada de aplicação. OBS:(Ainda necessita implementar a refatoração de logs)

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Aplicacao/ModuloPlanoDeCobranca/ServicoPlanoDeCobranca.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation.Results;
+using locadoraDeVeiculos.Infra.ModuloPlanoDeCobranca;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoDeCobranca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloPlanoDeCobranca
+{
+    public class ServicoPlanoDeCobranca
+    {
+        //TODO: Refatorar Em Log
+
+        private RepositorioPlanoDeCobrancaEmBancoDados repositorioPlanoDeCobranca;
+
+        public ServicoPlanoDeCobranca(RepositorioPlanoDeCobrancaEmBancoDados repositorioPlanoDeCobranca)
+        {
+            this.repositorioPlanoDeCobranca = repositorioPlanoDeCobranca;
+        }
+
+        public ValidationResult Inserir(PlanoDeCobranca arg)
+        {
+            var resultadoValidacao = ValidarPlanoDeCobranca(arg);
+
+            if (resultadoValidacao.IsValid)
+                repositorioPlanoDeCobranca.Inserir(arg);
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Editar(PlanoDeCobranca arg)
+        {
+            var resultadoValidacao = ValidarPlanoDeCobranca(arg);
+
+            if (resultadoValidacao.IsValid)
+                repositorioPlanoDeCobranca.Editar(arg);
+
+            return resultadoValidacao;
+        }
+
+        private ValidationResult ValidarPlanoDeCobranca(PlanoDeCobranca arg)
+        {
+            ValidadorPlanoDeCobranca validador = new ValidadorPlanoDeCobranca();
+
+            var resultadoValidacao = validador.Validate(arg);
+
+            return resultadoValidacao;
+        }
+    }
+}
