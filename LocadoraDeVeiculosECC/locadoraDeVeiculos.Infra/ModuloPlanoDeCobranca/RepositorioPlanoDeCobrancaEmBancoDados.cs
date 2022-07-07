650587a82d8c7407e6cf1554ebf451ab90af9db6
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-07 15:24:33 -0300

    Mapeador e Repositorio de Plano de Cobrança Implementado

---
+++ LocadoraDeVeiculosECC/locadoraDeVeiculos.Infra/ModuloPlanoDeCobranca/RepositorioPlanoDeCobrancaEmBancoDados.cs
@@ -0,0 +1,114 @@
+﻿using locadoraDeVeiculos.Infra.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoDeCobranca;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace locadoraDeVeiculos.Infra.ModuloPlanoDeCobranca
+{
+    public class RepositorioPlanoDeCobrancaEmBancoDados :
+        RepositorioBase<PlanoDeCobranca, MapeadorPlanoDeCobranca>,
+        IRepositorioPlanoDeCobranca
+    {
+        #region sql Queries
+
+        protected override string sqlInserir =>
+            @"INSERT INTO [TBPLANODECOBRANCA]
+                (
+                    [TIPODEPLANO], 
+                    [VALORDIARIO],
+                    [VALORKMINCLUSO],                    
+                    [PRECOKMRODADO],
+                    [GRUPODEVEICULOS_ID]
+                )
+            VALUES
+                (
+                    @TIPOPLANO,
+                    @VALORDIARIA,
+                    @KMINCLUSO,
+                    @PRECOKM,
+                    @GRUPODEVEICULOS_ID
+                ); SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+            @"UPDATE [TBPLANODECOBRANCA]
+                SET
+                    [TIPODEPLANO] = @TIPODEPLANO,
+                    [VALORDIARIO] = @VALORDIARIO,
+                    [VALORKMINCLUSO] = @VALORKMINCLUSO,
+                    [PRECOKMRODADO] = @PRECOKMRODADO,
+                    [GRUPODEVEICULOS_ID] = @GRUPODEVEICULOS_ID
+                WHERE
+                    [ID] = @ID";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM [TBPLANODECOBRANCA]
+                WHERE
+                    [ID] = @ID";
+
+        protected override string sqlSelecionarPorId =>
+            @"SELECT 
+                    [ID],
+		            [TIPODEPLANO], 
+                    [VALORDIARIO],
+                    [VALORKMINCLUSO],                    
+                    [PRECOKMRODADO],
+                    [GRUPODEVEICULOS_ID]  
+	            FROM 
+		            [TBPLANODECOBRANCA]
+		        WHERE
+                    [ID] = @ID";
+
+        protected override string sqlSelecionarTodos =>
+            @"SELECT 
+                    [ID],
+		            [TIPODEPLANO], 
+                    [VALORDIARIO],
+                    [VALORKMINCLUSO],                    
+                    [PRECOKMRODADO],
+                    [GRUPODEVEICULOS_ID]  
+	            FROM 
+		            [TBPLANODECOBRANCA]";
+
+        private string sqlSelecionarPorGrupo =>
+            @"SELECT
+                    [ID],
+		            [TIPODEPLANO], 
+                    [VALORDIARIO],
+                    [VALORKMINCLUSO],                    
+                    [PRECOKMRODADO],
+                    [GRUPODEVEICULOS_ID]    
+	            FROM 
+		            [TBPLANODECOBRANCA]
+                WHERE 
+                    [GRUPODEVEICULOS_ID] = @GRUPODEVEICULOS_ID";
+
+        private string sqlSelecionarPorTipoPlano =>
+            @"SELECT
+                    [ID],
+		            [TIPODEPLANO], 
+                    [VALORDIARIO],
+                    [VALORKMINCLUSO],                    
+                    [PRECOKMRODADO],
+                    [GRUPODEVEICULOS_ID]    
+	            FROM 
+		            [TBPLANODECOBRANCA]
+                WHERE 
+                    [TIPODEPLANO] = @TIPODEPLANO";
+        #endregion
+
+        public PlanoDeCobranca SelecionarPlanoPorGrupo(int id)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorGrupo, new SqlParameter("GRUPODEVEICULOS_ID", id));
+        }
+
+        public PlanoDeCobranca SelecionarPlanoPorTipoPlano(string tipoPlano)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorTipoPlano, new SqlParameter("TIPODEPLANO", tipoPlano));
+        }
+
+    }
+}
