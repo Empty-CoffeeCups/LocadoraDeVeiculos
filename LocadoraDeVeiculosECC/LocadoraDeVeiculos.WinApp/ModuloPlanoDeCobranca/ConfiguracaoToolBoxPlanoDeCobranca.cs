939430c4c0a78dfdeee062e7e920eb3b73c23e5f
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-09 14:56:37 -0300

    Implementado configuracao da toolbox de plano de cobrança

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloPlanoDeCobranca/ConfiguracaoToolBoxPlanoDeCobranca.cs
@@ -0,0 +1,25 @@
+﻿using LocadoraDeVeiculos.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.WinApp.ModuloPlanoDeCobranca
+{
+    public class ConfiguracaoToolBoxPlanoDeCobranca : ConfiguracaoToolBoxBase
+    {
+        public override string TipoCadastro => "Cadastro de Planos de Cobrança";
+
+        public override string TooltipInserir { get { return "Inserir um Plano de Cobrança"; } }
+
+        public override string TooltipEditar { get { return "Editar um Plano de Cobrança"; } }
+
+        public override string TooltipExcluir { get { return "Excluir um Plano de Cobrança"; } }
+
+        public override string TooltipAgrupar { get { return "Agrupar Planos de Cobrança"; } }
+
+        public override bool AgruparHabilitado { get { return true; } }
+
+    }
+}
