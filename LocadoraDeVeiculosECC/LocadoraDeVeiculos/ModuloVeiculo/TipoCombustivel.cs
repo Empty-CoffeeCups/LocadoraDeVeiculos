9d7b08bf9c950072c0db3e8a4d4738fe590f8dd2
daniel-martinez <danieldaol99@gmail.com>
2022-07-10 17:24:16 -0300

    Classe do veiculo

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos/ModuloVeiculo/TipoCombustivel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+
+namespace LocadoraDeVeiculos.Dominio.ModuloVeiculo
+{
+    public enum TipoCombustivel
+    {
+        [Description("Gasolina comum")]
+        GasolinaComum,
+
+        [Description("Gasolina aditivada")]
+        GasolinaAditivada,
+
+        [Description("Gasolina premium")]
+        GasolinaPremium,
+
+        [Description("Gasolina formulada")]
+        GasolinaFormulada,
+
+        [Description("Etanol")]
+        Etanol,
+
+        [Description("Etanol aditivado")]
+        EtanolAditivado,
+
+        [Description("GNV")]
+        Gnv,
+
+        [Description("Diesel")]
+        Diesel,
+
+        [Description("Diesel premium")]
+        DieselPremium,
+    }
+}
---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos/ModuloVeiculo/Veiculo.cs
@@ -0,0 +1,46 @@
+﻿using LocadoraDeVeiculos.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoDeVeiculos;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloVeiculo
+{
+    public class Veiculo : EntidadeBase<Veiculo>
+    {
+        public GrupoDeVeiculos GruposDeVeiculos { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public TipoCombustivel TipoDeCombustivel { get; set; }
+        public int CapacidadeDoTanque { get; set; }
+        public DateTime Ano { get; set; }
+        public int KmPercorrido { get; set; }
+        public Bitmap Foto { get; set; }
+
+        public Veiculo() { }
+
+        public Veiculo(GrupoDeVeiculos gruposDeVeiculos, string modelo, string marca, string placa, string cor, TipoCombustivel tipoDeCombustivel, int capacidadeDoTanque, DateTime ano, int kmPercorrido, Bitmap foto)
+        {
+            GruposDeVeiculos = gruposDeVeiculos;
+            Modelo = modelo;
+            Marca = marca;
+            Placa = placa;
+            Cor = cor;
+            TipoDeCombustivel = tipoDeCombustivel;
+            CapacidadeDoTanque = capacidadeDoTanque;
+            Ano = ano;
+            KmPercorrido = kmPercorrido;
+            Foto = foto;
+        }
+
+        public override void Atualizar(Veiculo registro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
