b06af11c8c8d88bffeae3af8fb7d05b4bc3a3835
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-26 09:44:25 -0300

    Implementado Compartilhado de Orm

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Infra.Orm/LocadoraDeVeiculosDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm
+{
+    public class LocadoraDeVeiculosDbContextFactory : IDesignTimeDbContextFactory<LocadoraDeVeiculosDbContext>
+    {
+        public LocadoraDeVeiculosDbContext CreateDbContext(string[] args)
+        {
+            var configuracao = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("ConfiguracaoAplicacao.json")
+             .Build();
+
+            var connectionString = configuracao.GetConnectionString("SqlServer");
+
+            return new LocadoraDeVeiculosDbContext(connectionString);
+        }
+    }
+}
