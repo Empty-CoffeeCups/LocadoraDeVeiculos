5080f95e8840d8d8ffc43795bb412ff69c8c2c2c
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-28 15:19:49 -0300

    Camada De Testes De Locacao Implementada (Ainda falta implementar métodos para testes)

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Dominio.Tests/ModuloLocacao/LocacaoTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.Tests.ModuloLocacao
+{
+    [TestClass]
+    public class LocacaoTest
+    {
+        public LocacaoTest()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("pt-BR");
+        }
+
+
+
+    }
+}
