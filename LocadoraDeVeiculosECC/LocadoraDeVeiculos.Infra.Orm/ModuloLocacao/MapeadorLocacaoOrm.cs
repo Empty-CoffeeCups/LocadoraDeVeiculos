f571f139a6d78e5389c5e998fe6c4077740cdedc
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-28 15:08:07 -0300

    Implementado Camada Orm (Infra) de Locacao - OBS: Ainda Implementar veiculo (merge)

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Infra.Orm/ModuloLocacao/MapeadorLocacaoOrm.cs
@@ -0,0 +1,42 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloLocacao
+{
+    public class MapeadorLocacaoOrm : IEntityTypeConfiguration<Locacao>
+    {
+        public void Configure(EntityTypeBuilder<Locacao> builder)
+        {
+            builder.ToTable("TBLocacao");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            
+            
+
+            builder.HasOne(x => x.Funcionario)
+                .WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Cliente)
+              .WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Condutor)
+                .WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            //     builder.HasOne(x => x.Veiculo)                     -- esperando o merge do modulo veiculo
+            //        .WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.PlanoDeCobranca)
+              .WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.Property(x => x.DataLocacao).IsRequired();
+            builder.Property(x => x.DataDevolucaoPrevista).IsRequired();
+            builder.Property(x => x.ValorTotalPrevisto).IsRequired();
+        }
+
+    }
+}
---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Infra.Orm/ModuloLocacao/RepositorioLocacaoOrm.cs
@@ -0,0 +1,57 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloLocacao
+{
+    public class RepositorioLocacaoOrm : IRepositorioLocacao
+    {
+        private DbSet<Locacao> locacoes;
+        private readonly LocadoraDeVeiculosDbContext dbContext;
+
+        public RepositorioLocacaoOrm(LocadoraDeVeiculosDbContext dbContext)
+        {
+            locacoes = dbContext.Set<Locacao>();
+            this.dbContext = dbContext;
+        }
+
+        public void Inserir(Locacao novoRegistro)
+        {
+            locacoes.Add(novoRegistro);
+        }
+
+        public void Editar(Locacao registro)
+        {
+            locacoes.Update(registro);
+        }
+
+        public void Excluir(Locacao registro)
+        {
+            locacoes.Remove(registro);
+        }
+
+        public Locacao SelecionarCondutorPorCliente(Guid cliente)
+        {
+            return locacoes.FirstOrDefault(x => x.Cliente.Id == cliente);
+        }
+        public Locacao SelecionarCondutorPorFuncionario(Guid funcionario)
+        {
+            return locacoes.FirstOrDefault(x => x.Funcionario.Id == funcionario);
+        }
+
+        public Locacao SelecionarPorId(Guid id)
+        {
+            return locacoes.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Locacao> SelecionarTodos()
+        {
+            return locacoes.ToList();
+        }
+
+    }
+}
