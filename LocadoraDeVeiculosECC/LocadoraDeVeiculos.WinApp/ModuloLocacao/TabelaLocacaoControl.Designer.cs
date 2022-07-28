ae6ad81b40736e035ae0548f71c5d7085be611d9
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-28 17:05:52 -0300

    WinApp (Controlador, Form, toolbox, control ) do modulo locacao implementado. (Ainda não testado na main)

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloLocacao/TabelaLocacaoControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LocadoraDeVeiculos.WinApp.ModuloLocacao
+{
+    partial class TabelaLocacaoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid
+            // 
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(3, 3);
+            this.Grid.Name = "Grid";
+            this.Grid.RowTemplate.Height = 25;
+            this.Grid.Size = new System.Drawing.Size(494, 311);
+            this.Grid.TabIndex = 0;
+            // 
+            // TabelaLocacaoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Grid);
+            this.Name = "TabelaLocacaoControl";
+            this.Size = new System.Drawing.Size(500, 317);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Grid;
+    }
+}
--- LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacaoForm.cs
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloLocacao/TelaCadastroLocacaoForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using FluentResults;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoDeCobranca;
+using LocadoraDeVeiculos.Dominio.ModuloTaxas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +19,125 @@ namespace LocadoraDeVeiculos.WinApp.ModuloLocacao
 {
     public partial class TelaCadastroLocacaoForm : Form
     {
-        public TelaCadastroLocacaoForm()
+        private Locacao locacao = new Locacao();
+        List<Taxas> taxas = new List<Taxas>();
+
+        public TelaCadastroLocacaoForm(List<Funcionario> funcionarios,List<Cliente> clientes, List<Condutor> condutores, /*List<Veiculo> veiculos,*/ List<PlanoDeCobranca> planos, List<Taxas> taxas)
         {
             InitializeComponent();
+            CarregarClientes(clientes);
+            CarregarFuncionarios(funcionarios);
+            CarregarCondutores(condutores);
+           // CarregarVeiculos(veiculos); -- esperando merge de modulo veiculo
+            CarregarPlanosDeCobranca(planos);
+            CarregarTaxas(taxas);
         }
+
+        public Func<Locacao, Result<Locacao>> GravarRegistro { get; set; }
+
+
+        public Locacao Locacao
+        {
+            get
+            {
+                return locacao;
+            }
+            set
+            {
+                locacao = value;
+
+                cmbCliente.SelectedItem = locacao.Cliente;
+                cmbFuncionario.SelectedItem = locacao.Funcionario;
+                cmbCondutor.SelectedItem = locacao.Condutor;
+                cmbPlanoDeCobranca.SelectedItem = locacao.PlanoDeCobranca;
+              //  cmbVeiculo.SelectedItem = locacao.Veiculo; -- esperando merge de modulo veiculo
+
+                dtpDataDeLocacao.Value = DateTime.Now.Date;
+                dtpDevolucaoPrevista.Value = DateTime.Now.Date;
+
+                //  txtKmRodado.Text = locacao.veiculo.kmRodado; -- esperando merge de modulo veiculo
+                //   txtGrupoDeVeiculos.Text = locacao.veiculo.GrupoDeVeiculo.NomeDoGrupo; -- esperando merge de modulo veiculo
+
+
+                txtValorTotalPrevisto.Enabled = false;
+                txtKmRodado.Enabled = false;
+                txtGrupoDeVeiculos.Enabled = false;
+
+                if (locacao.Taxas != null)
+                {
+                    foreach (var item in locacao.Taxas)
+                    {
+                        listTaxas.Items.Add(item);
+                    }
+                }
+
+
+            }
+        }
+
+        private void CarregarClientes(List<Cliente> clientes)
+        {
+            cmbCliente.Items.Clear();
+
+            foreach (var item in clientes)
+            {
+                cmbCliente.Items.Add(item);
+            }
+        }
+
+
+        private void CarregarFuncionarios(List<Funcionario> funcionarios)
+        {
+            cmbFuncionario.Items.Clear();
+
+            foreach (var item in funcionarios)
+            {
+                cmbFuncionario.Items.Add(item);
+            }
+        }
+
+
+        private void CarregarCondutores(List<Condutor> condutores)
+        {
+            cmbCondutor.Items.Clear();
+
+            foreach (var item in condutores)
+            {
+                cmbCondutor.Items.Add(item);
+            }
+        }
+
+        private void CarregarPlanosDeCobranca(List<PlanoDeCobranca> planos)
+        {
+            cmbPlanoDeCobranca.Items.Clear();
+
+            foreach (var item in planos)
+            {
+                cmbPlanoDeCobranca.Items.Add(item);
+            }
+        }
+
+        private void CarregarTaxas(List<Taxas> taxas)
+        {
+            listTaxas.Items.Clear();
+
+            foreach (var item in taxas)
+            {
+                listTaxas.Items.Add(item);
+            }
+        }
+
+        /*
+        private void CarregarVeiculos(List<Veiculo> veiculos) -- esperando merge de modulo veiculo
+        {
+            cmbVeiculo.Items.Clear();
+
+            foreach (var item in veiculos)
+            {
+                cbVeiculo.Items.Add(item);
+            }
+        }
+        */
+
     }
 }
