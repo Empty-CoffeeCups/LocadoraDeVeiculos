d38569888845f3f00dff949779581292bc7978fb
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-30 13:29:35 -0300

    WinApp Devolucao: ConfigToolBoxDevolucao Implementada

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloDevolucao/ConfiguracaoToolBoxDevolucao.cs
@@ -0,0 +1,24 @@
+﻿using LocadoraDeVeiculos.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
+{
+    public class ConfiguracaoToolBoxDevolucao : ConfiguracaoToolBoxBase
+    {
+        public override string TipoCadastro => "Cadastro de Devolucao";
+
+        public override string TooltipInserir { get { return "Inserir uma nova Devolucao"; } }
+
+        public override string TooltipEditar { get { return "Editar uma Devolucao existente"; } }
+
+        public override string TooltipExcluir { get { return "Excluir uma Devolucao existente"; } }
+
+        public override string TooltipAgrupar { get { return "Agrupar Devolucoes"; } }
+
+        public override bool AgruparHabilitado { get { return true; } }
+    }
+}
