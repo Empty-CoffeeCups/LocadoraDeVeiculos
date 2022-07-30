0306be26f185342cc6fca72c503794af377a2fff
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-30 13:17:51 -0300

    Implementado Dominio e Infra(Orm) do módulo de Devolucao

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos/ModuloDevolucao/IRepositorioDevolucao.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraDeVeiculos.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloDevolucao
+{
+    public interface IRepositorioDevolucao : IRepositorio<Devolucao>
+    {
+
+    }
+}
---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos/ModuloDevolucao/ValidadorDevolucao.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloDevolucao
+{
+    public class ValidadorDevolucao : AbstractValidator<Devolucao>
+    {
+        public ValidadorDevolucao()
+        {
+            RuleFor(x => x.Locacao)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(x => x.KmVeiculo)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.DataDeDevolucao)
+                .NotNull()
+                .NotEmpty();
+
+
+            RuleFor(x => x.NivelDoTanque)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThanOrEqualTo(0);
+
+            
+
+        }
+    }
+}
