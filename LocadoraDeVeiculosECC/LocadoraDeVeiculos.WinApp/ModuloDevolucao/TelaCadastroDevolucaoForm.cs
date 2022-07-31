b9548eb93819d0cf670020f847210e1e845de541
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-07-31 13:51:37 -0300

    Get e Set de Devolucao Implementado no WinApp

--- LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloDevolucao/TelaCadastroDevolucaoForm.cs
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.WinApp/ModuloDevolucao/TelaCadastroDevolucaoForm.cs
@@ -4,6 +4,7 @@ using LocadoraDeVeiculos.Aplicacao.ModuloPlanoDeCobranca;
 using LocadoraDeVeiculos.Aplicacao.ModuloTaxas;
 using LocadoraDeVeiculos.Dominio.ModuloDevolucao;
 using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoDeCobranca;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,17 +19,56 @@ namespace LocadoraDeVeiculos.WinApp.ModuloDevolucao
 {
     public partial class TelaCadastroDevolucaoForm : Form
     {
-        public Devolucao Devolucao = new Devolucao();
+        public Devolucao devolucao;
         private ServicoLocacao servicoLocacao;
+        private ServicoTaxa servicoTaxa;
+        private ServicoPlanoDeCobranca servicoPlanoDeCobranca;
+
+
         List<Locacao> locacoes;
+        List<PlanoDeCobranca> planoDeCobrancas; 
+        
 
         public TelaCadastroDevolucaoForm(ServicoLocacao servicoLocacao, ServicoTaxa servicoTaxa , ServicoPlanoDeCobranca servicoPlanoDeCobranca)
         {
             InitializeComponent();
             this.servicoLocacao = servicoLocacao;
+            this.servicoTaxa = servicoTaxa;
+            this.servicoPlanoDeCobranca = servicoPlanoDeCobranca;
+            planoDeCobrancas = servicoPlanoDeCobranca.SelecionarTodos().Value;
+            locacoes = servicoLocacao.SelecionarTodos().Value;
             locacoes = servicoLocacao.SelecionarTodos().Value;
         }
 
         public Func<Devolucao, Result<Devolucao>> GravarRegistro { get; set; }
+
+
+
+        public Devolucao Devolucao
+        {
+            get
+            {
+                return devolucao;
+            }
+            set
+            {
+                devolucao = value;
+                if (devolucao.Locacao != null)
+                {
+                    txtFuncionario.Text = devolucao.Locacao.Funcionario.Nome;
+                    txtCliente.Text = devolucao.Locacao.Cliente.Nome;
+                    // txtVeiculo.Text = devolucao.Locacao.Veiculo; -- esperando o merge do módulo veículo 
+                    // txtGrupoDeVeiculo.Text = devolucao.veiculo.GrupoDeVeiculo.NomeDoGrupo; --  -- esperando o merge do módulo veículo
+                    txtDataDeLocacao.Text = devolucao.Locacao.DataLocacao.ToShortDateString();
+                    txtDevolucaoPrevista.Text = devolucao.Locacao.DataDevolucaoPrevista.ToShortDateString();
+                    txtPlanoDeCobranca.Text = devolucao.Locacao.PlanoDeCobranca.ToString();
+
+                }
+                cmbLocacao.SelectedItem = devolucao.Locacao;
+                txtValorTotal.Text = devolucao.ValorTotal.ToString();
+
+            }
+        }
+
     }
 }
