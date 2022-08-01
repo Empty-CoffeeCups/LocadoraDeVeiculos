7ce5f8ba5f317cbfa4cf741d3d14803c7ff2c10c
daniel-martinez <danieldaol99@gmail.com>
2022-08-01 13:27:57 -0300

    Refatoração do veiculo ORM

---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Infra.Orm/ModuloVeiculo/MapeadorVeiculoOrm.cs
@@ -0,0 +1,32 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloVeiculo
+{
+    public class MapeadorVeiculoOrm : IEntityTypeConfiguration<Veiculo>
+    {
+        public void Configure(EntityTypeBuilder<Veiculo> builder)
+        {
+            builder.ToTable("TBVeiculo");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.HasOne(x => x.GruposDeVeiculos);
+            builder.Property(x => x.Modelo).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Marca).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Placa).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Cor).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.TipoDeCombustivel).IsRequired();
+            builder.Property(x => x.CapacidadeDoTanque).IsRequired();
+            builder.Property(x => x.Ano).IsRequired();
+            builder.Property(x => x.KmPercorrido).IsRequired();
+            builder.Property(x => x.Foto).HasColumnType("VARBINARY(MAX)").IsRequired();
+
+        }
+
+    }
+}
---
+++ LocadoraDeVeiculosECC/LocadoraDeVeiculos.Infra.Orm/ModuloVeiculo/RepositorioVeiculoOrm.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.Orm.ModuloVeiculo
+{
+    public class RepositorioVeiculoOrm : IRepositorioVeiculo
+    {
+        private DbSet<Veiculo> veiculos;
+        private readonly LocadoraDeVeiculosDbContext dbContext;
+
+        public RepositorioVeiculoOrm(LocadoraDeVeiculosDbContext dbContext)
+        {
+            veiculos = dbContext.Set<Veiculo>();
+            this.dbContext = dbContext;
+        }
+        public void Inserir(Veiculo novoRegistro)
+        {
+            veiculos.Add(novoRegistro);
+        }
+
+        public void Editar(Veiculo registro)
+        {
+            veiculos.Update(registro);
+        }
+
+        public void Excluir(Veiculo registro)
+        {
+            veiculos.Remove(registro);
+        }
+        public Veiculo SelecionarPorId(Guid id)
+        {
+            return veiculos.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Veiculo SelecionarVeiculoPorPlaca(string placa)
+        {
+            return veiculos.FirstOrDefault(x => x.Placa == placa);
+        }
+
+        public List<Veiculo> SelecionarTodos()
+        {
+            return veiculos.ToList();
+        }
+    }
+}
